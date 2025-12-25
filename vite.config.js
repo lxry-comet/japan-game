9fce6f7e896f3c2f748d9ac144346d0ad2507f1d
MrForestino <durdentagirowich@gmail.com>
2025-12-25 18:37:57 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  base: '/japan-gamehw-js-vite/', //! 👈 ВАЖЛИВО: вкажіть базу
+  base: '/japan-game/', //! 👈 ВАЖЛИВО: вкажіть базу
   root: 'src',
   build: {
     rollupOptions: {
